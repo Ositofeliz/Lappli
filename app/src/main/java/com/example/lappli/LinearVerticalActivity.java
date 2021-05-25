818c55e9845cf5ad6a34cc512766a0883387f278
Remy Costa <remy.costa@live.fr>
2021-05-25 14:21:03 +0200

    Refonte de la page d'accueil pour correspondre à l'énnoncé

--- app/src/main/java/com/example/lappli/LinearVerticalActivity.java
+++ app/src/main/java/com/example/lappli/LinearVerticalActivity.java
@@ -1,13 +1,26 @@
 package com.example.lappli;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LinearVerticalActivity extends AppCompatActivity {
+
+    private static final String TAG = "LinearLayout";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_vertical);
+
+        Intent intent = getIntent();
+        String titreVerticalLinearLayout = intent.getStringExtra("titre_VerticalLinearLayout");
+
+        Log.i(TAG, "onCreate: " + titreVerticalLinearLayout);
+
+        
+
     }
 }
